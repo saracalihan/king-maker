06d9ed0721040ab36669360554e13fcb03303583
Alihan SARAÇ(Brick) <saracalihan@gmail.com>
2024-07-30 23:45:31 +0300

    feat: lazy loading added in to many2one rels

--- src/main/java/com/example/tahakkum/model/Token.java
+++ src/main/java/com/example/tahakkum/model/Token.java
@@ -12,6 +12,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -26,7 +27,7 @@ public class Token {
     public String value;
 
     @JsonIgnore
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
 
